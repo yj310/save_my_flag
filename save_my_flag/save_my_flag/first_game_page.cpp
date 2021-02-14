5eaed1d6335f9bcd3d5f8416b79309abd3183949
kny <kny030303>
2021-02-14 16:49:37 +0900

    first_game_page background img

--- save_my_flag/save_my_flag/first_game_page.cpp
+++ save_my_flag/save_my_flag/first_game_page.cpp
@@ -14,7 +14,24 @@ void FirstGamePage::Update()
 
 void FirstGamePage::Render()
 {
+	TextureElement* element = textureManager.getTexture(TEX_FIRST_GAME_PAGE_BACKGROUND);
 
+	RECT rc;
+	D3DXVECTOR3 pos;
+	D3DXVECTOR3 cen;
+
+	element->sprite->Begin(D3DXSPRITE_ALPHABLEND);
+
+	rc.left = 0;
+	rc.top = 0;
+	rc.right = WINDOW_WIDTH;
+	rc.bottom = WINDOW_HEIGHT;
+
+	pos = { 0, 0, 0 };
+
+	element->sprite->Draw(element->texture, &rc, nullptr, &pos, D3DCOLOR_ARGB(255, 255, 255, 255));
+
+	element->sprite->End();
 }
 
 void FirstGamePage::BackgroundUpdate()
