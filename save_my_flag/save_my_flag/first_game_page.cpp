44ca31f05f0d39824546a98a6fe5c36b37613191
kny <kny030303>
2021-02-14 16:34:52 +0900

    First_game_page

---
+++ save_my_flag/save_my_flag/first_game_page.cpp
@@ -0,0 +1,28 @@
+#include "first_game_page.h"
+#include "global.h"	
+
+
+FirstGamePage::FirstGamePage()
+{
+	classType = FIRST_GAME_PAGE;
+}
+
+void FirstGamePage::Update()
+{
+
+}
+
+void FirstGamePage::Render()
+{
+
+}
+
+void FirstGamePage::BackgroundUpdate()
+{
+
+}
+
+void FirstGamePage::BackgroundRender()
+{
+
+}
