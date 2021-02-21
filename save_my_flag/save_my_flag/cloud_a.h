d2458cb43218ae9cb149a4f6b5cdc8250f742860
kny <kny030303>
2021-02-21 19:19:48 +0900

    cloud

---
+++ save_my_flag/save_my_flag/cloud_a.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "clooud.h"
+
+class CloudA : public Cloud
+{
+public:
+
+	CloudA(float x, float y, float direction);
+	void Render() override;
+	void Update() override;
+
+
+
+	D3DXVECTOR2 getPos() override;
+	void setPos(float x, float y) override;
+	D3DXVECTOR2 getSize() override;
+	void setSize(float width, float height) override;
+	int getState() override;
+	void setState(int state) override;
+	float getDirection() override;
+	void setDirection(float direction) override;
+	float getSpeed() override;
+	void setSpeed(float speed) override;
+
+	int state;
+	int state_count;
+
+	float posX;
+	float posY;
+
+	float width;
+	float height;
+
+	float direction;
+	float speed;
+
+};
